5029a19d4f16db582dca128b9440c1c08f730ee2
leipishu <kimi.peng@outlook.com>
2025-06-24 20:20:47 +0800

    github地址更新

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -38,7 +38,7 @@ export default defineConfig({
 
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/leipishu/ServerWebsiteNew' }
     ],
 
     footer: {
